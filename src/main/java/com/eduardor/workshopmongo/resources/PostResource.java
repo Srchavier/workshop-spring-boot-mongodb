e128007d230dbf218f40762e95a460175e84af24
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-09-09 21:55:33 -0300

    Consulta simples com query methods

--- src/main/java/com/eduardor/workshopmongo/resources/PostResource.java
+++ src/main/java/com/eduardor/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardor.workshopmongo.domain.Post;
+import com.eduardor.workshopmongo.resources.ultil.URL;
 import com.eduardor.workshopmongo.services.PostService;
 
 @RestController
@@ -27,6 +29,12 @@ public class PostResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		return ResponseEntity.ok().body(postService.findById(id));
+	}
+
+	@GetMapping("/titleseach")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		return ResponseEntity.ok().body(postService.findByTitle(text));
 	}
 
 }
--- src/main/java/com/eduardor/workshopmongo/services/PostService.java
+++ src/main/java/com/eduardor/workshopmongo/services/PostService.java
@@ -23,4 +23,8 @@ public class PostService {
 		return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+
 }
